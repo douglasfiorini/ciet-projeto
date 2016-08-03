891b46e36192a4d05e34bc13981a687eee357366
bernardons <bernardo.luks@gmail.com>
2016-08-03 11:13:10 -0300

    Repository

--- src/main/java/ceit/projeto/repository/CargaRepository.java
+++ src/main/java/ceit/projeto/repository/CargaRepository.java
@@ -1,5 +1,7 @@
 package ceit.projeto.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import ceit.projeto.CargaVO;
 
 public interface CargaRepository extends JpaRepository<CargaVO, Long> {
